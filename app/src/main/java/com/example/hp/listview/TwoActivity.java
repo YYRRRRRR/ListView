7af145bc5fafaec27fcccc74c518409a8d959e4f
YYRRRRRR <3454711833@qq.com>
2019-04-07 09:37:44 +0800

    实验三ListView

---
+++ app/src/main/java/com/example/hp/listview/TwoActivity.java
@@ -0,0 +1,14 @@
+package com.example.hp.listview;
+
+/**
+ * Created by hp on 2019/3/26.
+ */
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+public class TwoActivity extends AppCompatActivity{
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main5);
+    }
+}
